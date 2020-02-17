c34cd598fb52442133530ecbc3f0efba035f0a8e
admin <admin@192.168.7.120>
2020-02-17 10:53:31 +0530

    loan

--- java/src/com/Assignment2/LoanManagement.java
+++ java/src/com/Assignment2/LoanManagement.java
@@ -10,18 +10,21 @@ public class LoanManagement {
 	private long mobile;
 	private String email;
 	static int id = 0;
+	static int loanid = 1;
+	private static Scanner sc;
 	static String Customer[][] = new String[2][4];
 	public static void main(String[] args) {
 		
 		
 		System.out.println("XYZ Finance Company welcomes you\n\t1.Register Customer\n\t 2.Exit");
 		int ch=0;
-		Scanner sc= new Scanner(System.in);
+		sc= new Scanner(System.in);
 		while(true) {
 			ch=sc.nextInt();
 		switch(ch)
 		{
-		case 1: Registraion();
+		case 1: id++; 
+			Registraion(id);
 		break;
 		case 2:System.out.println("Exited"); 
 			System.exit(0);
@@ -31,8 +34,62 @@ public class LoanManagement {
 		}
 	}
 	
-	static void Registraion() 
+	static void Registraion(int id) 
 	{
+		System.out.println("Enter details:");
+
+		System.out.println("Name :");
+		Customer[id][0] = sc.next();
+
+		System.out.println("Mobile Number :");
+		Customer[id][1] = sc.next();
+
+		System.out.println("Address :");
+		Customer[id][2] = sc.next();
+
+		System.out.println("Customer Information saved successfully\nYour Customer Id is : XYZ_00" + id);
+
+		System.out.println("-------------------------------------");
+		System.out.println("do you eish to apply for loan? ans in Yes/no");
+		
+		String choice = sc.next();
+		if(choice.equalsIgnoreCase("yes"))
+		{
+			Loan();
+		}
+		else
+		{
+			System.out.println("Exit");
+			System.exit(0);
+		}
+	}
+	
+	static void Loan() {
+		System.out.println("Enter the loan amount");
+		double amount = sc.nextDouble();
+		System.out.println("Enter the loan duration in months");
+		int duration=sc.nextInt();
+		float r= (float)9.5/100;
+		double emi = amount * r * (1+r)* duration / ((1+r)*duration -1);
+		
+		System.out.println("Your EMI for "+ amount+" and duration is"+emi);
+		System.out.println(" You want to get the loan? reply in Yes/No");
+		String loanch=sc.nextLine();
+		if(loanch.equalsIgnoreCase("yes"))
+		{
+			System.out.println("Your loan id is "+ loanid);
+		}
+		else {
+			//int tempId = sc.nextInt();
+			System.out.println("Name :" + Customer[id][0]);
+
+			System.out.println("Mobile Number :" + Customer[id][1]);
+
+			System.out.println("Address :" + Customer[id][2]);
+		
+
+			System.out.println("-------------------------------------");
+		}
 		
 	}
 
