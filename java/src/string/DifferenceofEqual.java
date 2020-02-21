7857728e66f4a2d32c67f19448f889bebf2b855d
Amey97 <amey2721997@gmail.com>
2020-02-21 23:53:40 +0530

    Update DifferenceofEqual.java

--- java/src/string/DifferenceofEqual.java
+++ java/src/string/DifferenceofEqual.java
@@ -10,8 +10,8 @@ public class DifferenceofEqual {
 
 		String s4 = "Hello";
 		String s3 = "Hello";
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
+		System.out.println(s1 == s2);//ye address dekhta hai
+		System.out.println(s1.equals(s2));//ye value dekhta hai
 
 		System.out.println(s3 == s4);
 		System.out.println(s1 == s4);
